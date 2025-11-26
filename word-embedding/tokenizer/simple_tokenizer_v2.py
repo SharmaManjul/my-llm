cdb10d4dcdaeef651dd90625b06d948c6d5baa9c
manjul <manjul199912@gmail.com>
2025-11-26 08:36:24 -0800

    adding tokenizer_v2

---
+++ word-embedding/tokenizer/simple_tokenizer_v2.py
@@ -0,0 +1,26 @@
+import re
+
+
+class SimpleTokenizerV2:
+    """
+    Docstring for SimpleTokenizerV2:
+    The difference between V1 and V2 is that V2 handles special tokens <|endoftext|> and <|unk|>.
+    """
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i: s for s, i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed  = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [
+            item if item in self.str_to_int 
+            else "<|unk|>" for item in preprocessed
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    
+    def decode (self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text   
--- word-embedding/tokenizer/tokenizer.py
+++ word-embedding/tokenizer/tokenizer.py
@@ -1,5 +1,6 @@
 import re
 from simple_tokenizer_v1 import SimpleTokenizerV1
+from simple_tokenizer_v2 import SimpleTokenizerV2
 
 # Reading the raw text file
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
@@ -16,15 +17,17 @@ print("First 20 string tokens:", stringTokens[:30])
 print('\n')
 
 # Creating sorted vocabulary of unique tokens
-all_words = sorted(set(stringTokens))
+all_words = sorted(list(set(stringTokens)))
+all_words.extend(["<|endoftext|>", "<|unk|>"])
 vocab_size = len(all_words)
 print("Vocabulary size:", vocab_size)
 vocab = {token:integer for integer, token in enumerate(all_words)}
-for i, item in enumerate(list(vocab.items())[:6]):
+for i, item in enumerate(list(vocab.items())[-5:]):
     print(item)
 print('\n')
 
-# Using the tokenizer class to encode and decode
+# Using the tokenizer_v1 class to encode and decode
+print("Using SimpleTokenizerV1 to encode and decode:")
 tokenizer = SimpleTokenizerV1(vocab)
 sample_text = """"It's the last he painted, you know," 
            Mrs. Gisburn said with pardonable pride."""
@@ -32,3 +35,16 @@ ids = tokenizer.encode(sample_text)
 print("Ids of encoded sample text:", ids)
 decode_text = tokenizer.decode(ids)
 print("Decoded text from ids:", decode_text)
+print('\n')
+
+# Using the tokenizer_v2 class to encode and decode
+print("Using SimpleTokenizerV2 to encode and decode with unknown text:")
+tokenizer_v2 = SimpleTokenizerV2(vocab)
+
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+ids_v2 = tokenizer_v2.encode(text)
+print("Ids of encoded sample text:", ids_v2)
+decode_text_v2 = tokenizer_v2.decode(ids_v2)
+print("Decoded text from ids:", decode_text_v2)
